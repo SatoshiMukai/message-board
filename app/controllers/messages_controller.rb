129694244f0699ecbc982a315f7faf9f9461091b
SatoshiMukai <mukai2001@mx6.ttcn.ne.jp>
2016-04-27 07:48:16 +0000

    11

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,9 +6,10 @@ before_action :set_message, only: [:edit, :update]
     # Messageを全て取得する。
     @messages = Message.all
   end
+  
 def edit
 end
-  
+
 def update
     if @message.update(message_params)
       # 保存に成功した場合はトップページへリダイレクト
@@ -16,23 +17,6 @@ def update
     else
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
-    end
-end
-
-  def destroy
-    @message.destroy
-    redirect_to root_path, notice: 'メッセージを削除しました'
-  end
-
-def create
-    @message = Message.new(message_params)
-    if @message.save
-      redirect_to root_path , notice: 'メッセージを保存しました'
-    else
-      # メッセージが保存できなかった時
-      @messages = Message.all
-      flash.now[:alert] = "メッセージの保存に失敗しました。"
-      render 'index'
     end
 end
   
@@ -45,6 +29,12 @@ end
   def set_message
     @message = Message.find(params[:id])
   end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+
 
   ## ここまで
 end
